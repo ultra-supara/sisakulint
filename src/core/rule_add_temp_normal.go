5e307986282f709fcabcfb2c8e307c6cf5fbfde2
ultra-supara <atsu120225@gmail.com>
2023-11-06 17:38:43 +0900

    template normal

---
+++ src/core/rule_add_temp_normal.go
@@ -0,0 +1,41 @@
+// build対象外のテンプレートファイル
+package core
+
+import (
+	"github.com/ultra-supara/sisakulint/src/ast"
+)
+
+type AddRule struct {
+	BaseRule
+	//todo:必要に応じてここにルールを追加してください
+}
+
+// ConditionalRule は 新しいConditionalRuleを作ります
+func NewAddRule() *AddRule {
+	return &AddRule{
+		BaseRule: BaseRule{
+			RuleName: "add",
+			RuleDesc: "?",
+		},
+	}
+}
+
+// VisitStep is callback when visiting Step node.
+func (rule *AddRule) VisitStep(n *ast.Step) error {
+	//todo:ここに実装を追加してください
+	return nil
+}
+
+//VisitJobPre is called before visiting a job children
+func (rule *AddRule) VisitJobPre(n *ast.Job) error {
+	//todo:ここに実装を追加してください
+	return nil
+}
+
+//VisitJobPost is called after visiting a job children
+func (rule *AddRule) VisitJobPost(n *ast.Job) error {
+	//todo:ここに実装を追加してください
+	return nil
+}
+
+//todo : validateの関数を実装してください
