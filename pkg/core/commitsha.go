3fdae47687729b07d38f652cdff2bc993ace4644
on-keyday <62627905+on-keyday@users.noreply.github.com>
2024-12-30 01:16:29 +0900

    add commit-sha autofix

--- pkg/core/commitsha.go
+++ pkg/core/commitsha.go
@@ -1,8 +1,12 @@
 package core
 
 import (
+	"context"
+	"net/http"
 	"regexp"
+	"strings"
 
+	"github.com/google/go-github/v68/github"
 	"github.com/ultra-supara/sisakulint/pkg/ast"
 )
 
@@ -26,16 +30,80 @@ func isFullLengthSha(ref string) bool {
 }
 
 // VisitJobPre checks each step in each job for the action ref specifications
-func (rule *CommitSha) VisitJobPre(node *ast.Job) error {
-	for _, step := range node.Steps {
-		if action, ok := step.Exec.(*ast.ExecAction); ok {
-			usesValue := action.Uses.Value
-			if !isFullLengthSha(usesValue) {
-				rule.Errorf(step.Pos,
-					"the action ref in 'uses' for step '%s' should be a full length commit SHA for immutability and security. See documents: https://docs.github.com/en/actions/security-guides/security-hardening-for-github-actions#using-third-party-actions",
-					step.String())
-			}
+func (rule *CommitSha) VisitStep(step *ast.Step) error {
+	if action, ok := step.Exec.(*ast.ExecAction); ok {
+		usesValue := action.Uses.Value
+		if !isFullLengthSha(usesValue) {
+			rule.Errorf(step.Pos,
+				"the action ref in 'uses' for step '%s' should be a full length commit SHA for immutability and security. See documents: https://docs.github.com/en/actions/security-guides/security-hardening-for-github-actions#using-third-party-actions",
+				step.String())
+			rule.AddAutoFixer(NewStepFixer(step, rule)) // add autofix for this CommitSha rule
 		}
 	}
+	return nil
+}
+
+// from https://github.com/suzuki-shunsuke/pinact/blob/532aa7ba57db6c11937831f993b51640bbda94ac/pkg/controller/run/parse_line.go#L18-L19
+var (
+	semverPattern   = regexp.MustCompile(`^v?\d+\.\d+\.\d+[^ ]*$`)
+	shortTagPattern = regexp.MustCompile(`^v\d+$`)
+)
+
+func getLongVersion(cl *github.Client, owner, repo, sha string, expectedTag string) (string, error) {
+	opts := &github.ListOptions{
+		PerPage: 100,
+	}
+	for i := 0; i < 10; i++ {
+		tags, resp, err := cl.Repositories.ListTags(context.Background(), owner, repo, opts)
+		if err != nil {
+			return "", err
+		}
+		for _, tag := range tags {
+			if tag.GetCommit().GetSHA() == sha {
+				tagName := tag.GetName()
+				if strings.HasPrefix(tagName, expectedTag) {
+					return tagName, nil
+				}
+			}
+		}
+		if resp.NextPage == 0 {
+			break
+		}
+		opts.Page = resp.NextPage
+	}
+	return "", nil
+}
+
+func (rule *CommitSha) FixStep(step *ast.Step) error {
+	// at here, we can assume that the action ref is not a full length commit SHA
+	action := step.Exec.(*ast.ExecAction)
+	usesValue := action.Uses.Value
+	gh := github.NewClient(http.DefaultClient)
+	splitTag := strings.Split(usesValue, "@")
+	ownerRepo := strings.Split(splitTag[0], "/")
+	if len(ownerRepo) != 2 {
+		return nil // cannot fix...
+	}
+	tag := splitTag[1]
+	isSemver := semverPattern.MatchString(splitTag[1])
+	isShortTag := shortTagPattern.MatchString(splitTag[1])
+	//tagComment := action.Uses.BaseNode.LineComment
+	sha, _, err := gh.Repositories.GetCommitSHA1(context.TODO(), ownerRepo[0], ownerRepo[1], tag, "")
+	if err != nil {
+		// temporary report error???
+		rule.Errorf(step.Pos,
+			"tag '%s' is not found in the repository '%s' at step '%s'",
+			splitTag[1], splitTag[0], step.String())
+		return nil
+	}
+	if !isSemver && isShortTag {
+		longVersion, err := getLongVersion(gh, ownerRepo[0], ownerRepo[1], sha, splitTag[1])
+		if err != nil {
+			return err
+		}
+		tag = longVersion
+	}
+	action.Uses.BaseNode.Value = splitTag[0] + "@" + sha
+	action.Uses.BaseNode.LineComment = tag
 	return nil
 }
